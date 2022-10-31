4f605843dbb3c3945c4929c22b98587ce22e45b7
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-10-31 10:45:35 +0100

    first commit

---
+++ src/test/java/de/samples/todos/SpringFeaturesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.samples.todos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFeaturesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
