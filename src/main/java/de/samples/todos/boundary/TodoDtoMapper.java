857e20caa7a8117c1829e387a297b813d031dedc
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-10-31 23:33:08 +0100

    Introduce custom DTO for HTTP transport

---
+++ src/main/java/de/samples/todos/boundary/TodoDtoMapper.java
@@ -0,0 +1,31 @@
+package de.samples.todos.boundary;
+
+import de.samples.todos.domain.Todo;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface TodoDtoMapper {
+
+    TodoDto map(Todo todo);
+    Todo map(TodoDto todo);
+
+    default String mapStatus(Todo.TodoStatus status) {
+        return switch (status) {
+            case NEW -> "new";
+            case PROGRESS -> "in_progress";
+            case CANCELLED -> "cancelled";
+            case COMPLETED -> "completed";
+        };
+    }
+
+    default Todo.TodoStatus mapStatus(String status) {
+        return switch (status) {
+            case "new" -> Todo.TodoStatus.NEW;
+            case "in_progress" -> Todo.TodoStatus.PROGRESS;
+            case "cancelled" -> Todo.TodoStatus.CANCELLED;
+            case "completed" -> Todo.TodoStatus.COMPLETED;
+            default -> null;
+        };
+    }
+
+}
