8dc6095cdd84fb8d34bb6a73640b91210d49992a
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-10-31 13:10:30 +0100

    Add HelloWorld Controller

---
+++ src/main/java/de/samples/todos/boundary/HelloWorldController.java
@@ -0,0 +1,23 @@
+package de.samples.todos.boundary;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("hello")
+public class HelloWorldController {
+
+    @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
+    @ResponseBody
+    String sayHello(
+      @RequestParam(value = "name", defaultValue = "World")
+      String name
+    ) {
+        return String.format("Hello %s!", name);
+    }
+
+}
