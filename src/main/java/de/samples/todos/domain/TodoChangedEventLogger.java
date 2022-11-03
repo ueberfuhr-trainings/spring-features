01bd3f48f9f465c3fbd96abcaf32d2bbb294ebf6
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-11-03 10:12:56 +0100

    Only activate event logger in dev profile

--- src/main/java/de/samples/todos/domain/TodoChangedEventLogger.java
+++ src/main/java/de/samples/todos/domain/TodoChangedEventLogger.java
@@ -1,11 +1,13 @@
 package de.samples.todos.domain;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@Profile("dev")
 public class TodoChangedEventLogger {
 
     @EventListener(TodoChangedEvent.class)
