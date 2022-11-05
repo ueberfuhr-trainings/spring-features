4e447a2f57915f99cc50ca044656efa3ed63cd2c
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-11-06 00:02:03 +0100

    Add Actuator

--- src/main/java/de/samples/todos/SpringFeaturesDemoApplication.java
+++ src/main/java/de/samples/todos/SpringFeaturesDemoApplication.java
@@ -2,12 +2,18 @@ package de.samples.todos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class SpringFeaturesDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringFeaturesDemoApplication.class, args);
+		// SpringApplication.run(SpringFeaturesDemoApplication.class, args);
+		// Necessary for Actuator Startup Endpoint
+		final var app = new SpringApplication(SpringFeaturesDemoApplication.class);
+		app.setApplicationStartup(new BufferingApplicationStartup(2048));
+		app.run(args);
+
 	}
 
 }
